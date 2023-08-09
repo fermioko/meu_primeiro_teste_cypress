59f26e8f32fd93348f66742f92bcfebf7cfbadf2
fermioko <mioko.senac.2023@gmail.com>
2023-08-09 20:43:11 -0300

    cypress

---
+++ cypress/e2e/login-senac.cy.js
@@ -0,0 +1,16 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.sp.senac.br')
+
+    cy.get('.mburger').click()
+
+    cy.get('#txtLoginNaoLogadoMob').click()
+
+    cy.get('#login-email').type('janedoe@example.com')
+
+    cy.get('#login-password').type('example123')
+
+    cy.get('#btnLoginHeader').click()
+
+  })
+})
